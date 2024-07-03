1f8a3635086f610640aad6f36a0d3d1140801175
Salman <138432133+Sa1man9@users.noreply.github.com>
2024-07-03 17:54:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://deploy-mern-1whq.vercel.app",
         credentials:true
     })
 )
