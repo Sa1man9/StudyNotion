fed80c52d7323de8658088248039d2856fbf4a02
Salman <138432133+Sa1man9@users.noreply.github.com>
2024-07-03 19:33:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.use(cookieParser())
 app.use(
     cors({
         origin: ["https://study-notion-frontend-eight-khaki.vercel.app"],
+        methods: ["POST", "GET", "DELETE"],
         credentials:true
     })
 )
